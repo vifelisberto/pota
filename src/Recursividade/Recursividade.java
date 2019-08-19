c1cd76d179f95407f109dd38cb23f4e4998759ec
vifelisberto <vi.felis@hotmail.com>
2019-08-18 23:37:26 -0300

    Contagem regressiva recursiva

--- src/Recursividade/Recursividade.java
+++ src/Recursividade/Recursividade.java
@@ -9,11 +9,14 @@ import java.io.File;
 public class Recursividade {
 
     public static void main(String[] args) {
-        listaConteudo(new File(".")); // Diretório corrente
+        //listaConteudo(new File(".")); // Diretório corrente
+        contagemRegressiva(10);
     }
 
     /**
-     * Método que lista o conteudo de terminado diretório, exibindo todos os arquivos/Pastas.
+     * Método que lista o conteudo de terminado diretório, exibindo todos os
+     * arquivos/Pastas.
+     *
      * @param arquivo Diretório corrente para busca.
      */
     private static void listaConteudo(File arquivo) {
@@ -26,6 +29,20 @@ public class Recursividade {
             }
         } else {
             System.out.println("Arquivo " + arquivo.getName());
+        }
+    }
+
+    /**
+     * Método de contagem Regressiva Recursiva.
+     *
+     * @param n Número que irá começar a contagem regressiva.
+     */
+    private static void contagemRegressiva(int n) {
+        if (n == 0) {
+            System.out.println("0");
+        } else {
+            System.out.println(n);
+            contagemRegressiva(n - 1);
         }
     }
 }
